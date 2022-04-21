95a8ea5783855b58722c9748c6c36f7559f390a1
Mario Vidović <60817192+mariovida@users.noreply.github.com>
2022-04-21 22:21:41 +0200

    Add files via upload

--- js/slide.js
+++ js/slide.js
@@ -23,6 +23,34 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
+const scrollElements = document.querySelectorAll(".js-scroll");
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    }
+  })
+}
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
+
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
